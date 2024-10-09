64f94785cc569042c33827a4d7ba351ba91b0222
Amir Shehata <shehataa@ornl.gov>
2024-10-09 13:56:07 -0400

    QFw: Specify the circuit type with supermarq
    Added a --method argument which can take the type of circuit to create
    with the supermarq test app. Currently it supports ghz and vqe.
    
    The test qfw.batch uses ghz
    
    Signed-off-by: Amir Shehata <shehataa@ornl.gov>

--- examples/qfw_supermarq.sh
+++ examples/qfw_supermarq.sh
@@ -9,7 +9,7 @@ set -xe
 
 qfw_setup.sh
 
-run_application.sh "$QFW_PATH/../applications/test_supermarq.py" --run $1 --iterations $2 --startqbit $3 --increase $4
+run_application.sh "$QFW_PATH/../applications/test_supermarq.py" --run $1 --iterations $2 --startqbit $3 --increase $4 --method $5
 
 qfw_teardown.sh
 
