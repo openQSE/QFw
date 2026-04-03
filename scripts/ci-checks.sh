01d163b9b85b475e22d96854e11cdca55e4de983
Thomas Naughton <naughtont@ornl.gov>
2026-04-03 15:23:49 -0400

    TESTING - add local flake8 and modify ci-checks.sh
    THIS IS JUST TO TEST IF DEFw flake8 will clear remaining
    CI TEST ITEMS WHEN THAT LANDS.  THIS IS ONLY A HACK FOR TESTING.
    
    Signed-off-by: Thomas Naughton <naughtont@ornl.gov>

--- scripts/ci-checks.sh
+++ scripts/ci-checks.sh
@@ -13,7 +13,8 @@ set -e
 FLAKE8_DIRS="services/ service-apis/ backends/ examples/"
 
 echo "--- flake8 lint ---"
-flake8 --config DEFw/.flake8 $FLAKE8_DIRS
+#flake8 --config DEFw/.flake8 $FLAKE8_DIRS
+flake8 --config scripts/.flake8 $FLAKE8_DIRS
 
 echo "--- syntax check ---"
 find $FLAKE8_DIRS -name "*.py" -print0 | xargs -0 python -m py_compile
