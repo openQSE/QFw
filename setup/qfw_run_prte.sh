584f964dd0c5243cf28eeec88232b2f83659f31a
Amir Shehata <shehataa@ornl.gov>
2026-04-27 16:54:04 -0400

    QFw: Allow PRTE to run as root in containers
    Detect when qfw_run_prte.sh is running as root and append the PRTE
    allow-run-as-root flag only in that case. This keeps the existing launch
    path unchanged for non-root runs while allowing the containerized Slurm
    environment to start PRTE successfully from root-owned shells.
    
    Signed-off-by: Amir Shehata <shehataa@ornl.gov>

--- setup/qfw_run_prte.sh
+++ setup/qfw_run_prte.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
-export SLURM_JOBID=$3
-export SLURM_JOB_ID=$3
-echo "mkdir -p $1; prte --host $2 --report-uri $1/dvm-uri -x SLURM_JOB_ID=$3 -x SLURM_JOBID=$3 --prtemca ras ^slurm --prtemca plm slurm --prtemca plm_slurm_verbose 100 --prtemca plm_base_verbose 100 --prtemca ras_base_verbose 100 --prtemca plm_slurm_args '--het-group 1'"
 
-mkdir -p $1; prte --host $2 --report-uri $1/dvm-uri -x SLURM_JOB_ID=$3 -x SLURM_JOBID=$3 --prtemca ras ^slurm --prtemca plm slurm --prtemca plm_slurm_verbose 100 --prtemca plm_base_verbose 100 --prtemca ras_base_verbose 100 --prtemca plm_slurm_args "--het-group 1" --daemonize
+set -euo pipefail
 
-#--pmixmca pmix_server_spawn_verbose 100 --pmixmca pmix_client_spawn_verbose 100 
+export SLURM_JOBID="$3"
+export SLURM_JOB_ID="$3"
+
+PRTE_ARGS=(
+    --host "$2"
+    --report-uri "$1/dvm-uri"
+    -x "SLURM_JOB_ID=$3"
+    -x "SLURM_JOBID=$3"
+    --prtemca ras ^slurm
+    --prtemca plm slurm
+    --prtemca plm_slurm_verbose 100
+    --prtemca plm_base_verbose 100
+    --prtemca ras_base_verbose 100
+    --prtemca plm_slurm_args "--het-group 1"
+)
+
+if [ "$(id -u)" -eq 0 ]; then
+    PRTE_ARGS+=(--allow-run-as-root)
+fi
+
+echo "mkdir -p $1; prte ${PRTE_ARGS[*]} --daemonize"
+
+mkdir -p "$1"
+prte "${PRTE_ARGS[@]}" --daemonize
+
+# --pmixmca pmix_server_spawn_verbose 100 --pmixmca pmix_client_spawn_verbose 100
 # Below command has more debugs
-#mkdir -p $1; prte --debug-daemons --debug-daemons-file --host $2 --report-uri $1/dvm-uri -x SLURM_JOB_ID=$3 -x SLURM_JOBID=$3 --prtemca ras ^slurm --prtemca plm slurm --prtemca plm_slurm_verbose 100 --prtemca plm_base_verbose 100 --prtemca ras_base_verbose 100 --prtemca pmix_server_verbose 100  --prtemca odls_base_verbose 100 --prtemca prte_state_base_verbose 100 --prtemca plm_slurm_args "--het-group 1"
-#--daemonize
+# mkdir -p "$1"; prte --debug-daemons --debug-daemons-file "${PRTE_ARGS[@]}" \
+#     --prtemca pmix_server_verbose 100 --prtemca odls_base_verbose 100 \
+#     --prtemca prte_state_base_verbose 100 --daemonize
