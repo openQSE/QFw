b4a9d945acf865caaecec530f80a6ab853990e9a
Amir Shehata <shehataa@ornl.gov>
2026-08-06 11:03:31 -0400

    api_qpm: expose phase 3 tokenized category APIs
    Commit: PH3-C01 PH3-C02 PH3-C03 PH3-C05
    Plan: PH3.1 PH3.2 PH3.3 PH3.6 PH3.7
    Reqs: CAT-001 CAT-002 CAT-003 CAT-004 CAT-005 CAT-006 CAT-007
    Reqs: API-001 API-002 API-003 API-004
    Design: QFw API Categories; Token Placeholder For Current Milestone
    Design: Admission Policy Configuration APIs; Scheduler Control APIs
    Design: Admission Control APIs; Execution APIs
    Design: Telemetry And Discovery APIs
    Expose token placeholders and workflow-level category method shapes for
    execution, admission, admission policy, scheduler, and telemetry RPC surfaces.
    
    Signed-off-by: Amir Shehata <shehataa@ornl.gov>

--- service-apis/api_qpm/api_qpm.py
+++ service-apis/api_qpm/api_qpm.py
@@ -25,14 +25,14 @@ class QPMRemoteBase(BaseRemote):
 
 
 class QPMExecution(QPMRemoteBase):
-	def delete_circuit(self, cid, reservation_id=None):
+	def delete_circuit(self, cid, reservation_id=None, token=None):
 		pass
 
-	def sync_run(self, info, reservation_id=None, timeout=None,
+	def sync_run(self, info, reservation_id=None, token=None, timeout=None,
 				 cancel_on_timeout=False):
 		pass
 
-	def async_run(self, info, reservation_id=None, timeout=None,
+	def async_run(self, info, reservation_id=None, token=None, timeout=None,
 				  cancel_on_timeout=False):
 		pass
 
@@ -45,128 +45,171 @@ class QPMExecution(QPMRemoteBase):
 	def is_ready(self):
 		pass
 
-	def read_cq(self, cid=None, reservation_id=None):
+	def read_cq(self, cid=None, reservation_id=None, token=None):
 		pass
 
-	def peek_cq(self, cid=None, reservation_id=None):
+	def peek_cq(self, cid=None, reservation_id=None, token=None):
 		pass
 
 	def register_event_notification(self, ep, evtype, class_id,
-					reservation_id=None, filters=None):
+					token=None, reservation_id=None, filters=None):
 		pass
 
-	def cancel_task(self, cid=None, qtask_id=None, reservation_id=None,
-			reason=None):
+	def cancel_task(self, cid=None, reservation_id=None, token=None,
+			reason=None, qtask_id=None):
 		pass
 
-	def task_status(self, cid=None, qtask_id=None, reservation_id=None):
+	def task_status(self, cid=None, reservation_id=None, token=None,
+		    qtask_id=None):
 		pass
 
 
 class QPMAdmissionControl(QPMRemoteBase):
-	def evaluate(self, request):
+	def evaluate(self, token=None, request=None):
 		pass
 
-	def reserve(self, request):
+	def reserve(self, token=None, request=None):
 		pass
 
-	def renew(self, reservation_id, request=None):
+	def renew(self, token=None, reservation_id=None, request=None):
 		pass
 
-	def release(self, reservation_id, reason=None):
+	def release(self, token=None, reservation_id=None, reason=None):
 		pass
 
-	def cancel(self, reservation_id, reason=None):
+	def cancel(self, token=None, reservation_id=None, reason=None):
 		pass
 
-	def get_reservation(self, reservation_id):
+	def get_reservation(self, token=None, reservation_id=None):
 		pass
 
-	def list_reservations(self, filters=None):
+	def list_reservations(self, token=None, filters=None):
 		pass
 
 
 class QPMAdmissionPolicyConfig(QPMRemoteBase):
-	def get_admission_policy(self):
+	def configure_device_profile(self, token=None, device_id=None,
+				     profile=None):
 		pass
 
-	def set_admission_policy(self, policy):
+	def get_device_profile(self, token=None, device_id=None):
 		pass
 
-	def get_capacity_model(self):
+	def configure_admission_policy(self, token=None, device_id=None,
+				       policy_name=None, policy_options=None,
+				       estimator_name=None,
+				       estimator_options=None):
 		pass
 
-	def set_capacity_model(self, capacity_model):
+	def get_admission_policy(self, token=None, device_id=None):
+		pass
+
+	def set_admission_policy(self, policy, token=None, device_id=None):
+		pass
+
+	def get_capacity_model(self, token=None, device_id=None):
+		pass
+
+	def set_capacity_model(self, token=None, device_id=None,
+			   capacity_model=None):
+		pass
+
+	def set_estimator_policy(self, estimator, token=None, device_id=None):
+		pass
+
+	def get_estimator_policy(self, token=None, device_id=None):
 		pass
 
 
 class QPMSchedulerControl(QPMRemoteBase):
-	def get_scheduler_status(self):
+	def configure_scheduler_policy(self, token=None, device_id=None,
+				       policy_name=None, policy_options=None):
 		pass
 
-	def get_scheduler_policy(self):
+	def get_scheduler_status(self, token=None, device_id=None):
 		pass
 
-	def set_scheduler_policy(self, policy):
+	def get_scheduler_policy(self, token=None, device_id=None):
 		pass
 
-	def pause(self, target_id=None, reason=None):
+	def set_scheduler_policy(self, policy, token=None, device_id=None):
 		pass
 
-	def resume(self, target_id=None):
+	def pause_execution_target(self, token=None, device_id=None,
+				   reason=None):
 		pass
 
-	def drain(self, target_id=None, mode="graceful", timeout_s=None):
+	def resume_execution_target(self, token=None, device_id=None):
 		pass
 
-	def set_dispatch_depth(self, max_inflight, target_id=None):
+	def drain_execution_target(self, token=None, device_id=None,
+				   mode="graceful", timeout_s=None):
 		pass
 
-	def get_scheduler_queue_state(self, target_id=None,
+	def pause(self, target_id=None, reason=None, token=None):
+		pass
+
+	def resume(self, target_id=None, token=None):
+		pass
+
+	def drain(self, target_id=None, mode="graceful", timeout_s=None,
+		  token=None):
+		pass
+
+	def set_dispatch_depth(self, token=None, device_id=None,
+			   max_inflight=None):
+		pass
+
+	def get_scheduler_queue_state(self, token=None, device_id=None,
 				      include_restricted=False):
 		pass
 
 
 class QPMTelemetry(QPMRemoteBase):
-	def get_backend_info(self, lib=None):
+	def get_backend_info(self, lib=None, token=None):
 		pass
 
-	def get_device_info(self, lib=None):
+	def get_device_info(self, lib=None, token=None):
 		pass
 
-	def get_dynamic_backend_info(self, calibration_set_id=None, lib=None):
+	def get_dynamic_backend_info(self, calibration_set_id=None, lib=None,
+				     token=None):
 		pass
 
-	def get_calibration_snapshot(self, calibration_set_id=None, lib=None):
+	def get_calibration_snapshot(self, calibration_set_id=None, lib=None,
+				     token=None):
 		pass
 
-	def get_coupling_graph(self, calibration_set_id=None, lib=None):
+	def get_coupling_graph(self, calibration_set_id=None, lib=None,
+			       token=None):
 		pass
 
-	def get_last_job_timing(self, cid=None, lib=None):
+	def get_last_job_timing(self, cid=None, lib=None, reservation_id=None,
+				token=None):
 		pass
 
-	def get_last_job_metadata(self, cid=None, lib=None):
+	def get_last_job_metadata(self, cid=None, lib=None, reservation_id=None,
+				  token=None):
 		pass
 
-	def get_task_metadata(self, cid=None, qtask_id=None,
-			      reservation_id=None):
+	def get_task_metadata(self, token=None, cid=None, reservation_id=None,
+				      qtask_id=None):
 		pass
 
-	def get_telemetry_access_model(self):
+	def get_telemetry_access_model(self, token=None):
 		pass
 
-	def get_capacity_snapshot(self, device_id=None, scope_id=None,
+	def get_capacity_snapshot(self, token=None, device_id=None, scope_id=None,
 				  access_class=None):
 		pass
 
-	def get_queue_metrics(self, device_id=None, access_class=None):
+	def get_queue_metrics(self, token=None, device_id=None, access_class=None):
 		pass
 
-	def reconcile_runtime_state(self, now_ns=None):
+	def reconcile_runtime_state(self, token=None, now_ns=None):
 		pass
 
-	def get_service_lifecycle_telemetry(self, access_class=None):
+	def get_service_lifecycle_telemetry(self, token=None, access_class=None):
 		pass
 
 	def test(self):
